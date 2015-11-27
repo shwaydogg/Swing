ba1d0d791fc53533937163d4435fc0291357430a
Anders Ramsay <andersr@gmail.com>
2015-11-27 15:54:52 -0500

    render content

---
+++ client/main.jsx
@@ -0,0 +1,16 @@
+App = React.createClass({
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+ 
+        <ul>
+          Line
+        </ul>
+      </div>
+    );
+  }
+});
