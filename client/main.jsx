39496977267769217001e311e3804fab5e4b5d82
Anders Ramsay <andersr@gmail.com>
2015-11-27 16:17:28 -0500

    saving to db

--- client/main.jsx
+++ client/main.jsx
@@ -1,17 +1,48 @@
 App = React.createClass({
- 
   render() {
     return ( 
       <div className="container">
         <header>
           <h1>New Node</h1>
         </header>
-          <form>
-          <input type="text" value="title" placeholder="title" />
-          <input type="text" value="content" placeholder="content" />
+          <form onSubmit={this.handleSubmit}>
+          <input
+            type="text" 
+            ref="titleInput"
+            placeholder="title" 
+          />
+          <input
+            type="text"
+            ref="contentInput"
+            placeholder="content" />
+
           <input type="submit" />
         </form>
       </div>
-    );
+  )},
+  handleSubmit(event) {
+    debugger;
+    console.log("submitted form");
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    var title = React.findDOMNode(this.refs.titleInput).value.trim();
+    var content = React.findDOMNode(this.refs.contentInput).value.trim();
+    
+    var node = new Node();
+
+    node.set({
+      title: title,
+      content: content,
+      createdAt: new Date() // current time
+    });
+
+    debugger;
+
+    node.save();
+ 
+    // Clear form
+    React.findDOMNode(this.refs.titleInput).value = "";
+    React.findDOMNode(this.refs.contentInput).value = "";
   }
 });
