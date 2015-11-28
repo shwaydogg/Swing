38172c024d6ab3d934c17c7b244d55933f26194d
Gregory Schwedock <schwedock@gmail.com>
2015-11-28 15:14:15 -0500

    Tiggered delete event

--- client/node.jsx
+++ client/node.jsx
@@ -5,8 +5,25 @@ Item = React.createClass({
   //  title: React.PropTypes.object.isRequired
   //},
   render() {
+    //console.log("in Item render", this);
     return (
-      <li>{this.props.item.title}</li>
+      <li>
+        {this.props.item.title}
+        <DeleteBtn/>
+      </li>
     );
   }
 });
+
+var DeleteBtn = React.createClass({
+  handleClick: function(event) {
+    event.preventDefault();
+    console.log('DELETED');
+  },
+  render() {
+    return (
+      <button onClick={this.handleClick}>&times;</button>
+    );
+  }
+});
+
