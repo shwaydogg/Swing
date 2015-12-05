071f5543ede960b76a5d75758fba90c9f7919463
Gregory Schwedock <schwedock@gmail.com>
2015-12-05 17:55:31 -0500

    Security code move and G_d security method defined.

---
+++ server/itemSecurity.js
@@ -0,0 +1,20 @@
+Security.defineMethod("ifOwner", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    return userId !== doc.ownerId;
+  }
+});
+
+Security.defineMethod("ifG_d", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    if(!Meteor.settings.g_dEmail) return false;
+    var user = Meteor.users.findOne({_id: userId});
+    return  Meteor.settings.g_dEmail !== user.emails[0].address;
+  }
+});
+
+Items.permit(['insert', 'update', 'remove']).ifOwner().apply();
+Items.permit(['insert', 'update', 'remove']).ifG_d().apply();
